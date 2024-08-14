ddc3ec4abeccee5b63b7a3cdb02cb8505956ce50
Naeemgg <naeemqg1@gmail.com>
2024-08-14 12:47:17 +0530

    shadcn configured add new favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      
+      <Button>Click me</Button>
     </div>
   )
 }
