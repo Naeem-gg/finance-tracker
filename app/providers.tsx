b250a43f7cde6bb9c909204722a8b79387fdfc5b
Naeemgg <naeemqg1@gmail.com>
2024-08-19 12:14:59 +0530

    add dark mode

---
+++ app/providers.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { type ThemeProviderProps } from "next-themes/dist/types"
+
+export default function Providers({ children }: ThemeProviderProps) {
+  return <NextThemesProvider  attribute="class"
+  defaultTheme="system"
+  enableSystem
+  disableTransitionOnChange>{children}</NextThemesProvider>
+}
